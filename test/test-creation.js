292b1410b2ccb154b13d99972358e98656885b1c
Tak Tran <contact@tutaktran.com>
2013-08-02 12:38:07 +0100

    Update tests with more files.

--- test/test-creation.js
+++ test/test-creation.js
@@ -19,15 +19,39 @@ describe('starttter generator', function () {
     }.bind(this));
   });
 
-  it('creates expected files', function (done) {
+  it('creates expected project files', function (done) {
     var expected = [
       // add files you expect to exist here.
+      '.bowerrc',
+      '.editorconfig',
+      '.gitignore',
       '.jshintrc',
-      '.editorconfig'
+      '.travis.yml'
     ];
 
     helpers.mockPrompt(this.app, {
-      'someOption': 'Y'
+      'websiteName': 'Some website'
+    });
+    this.app.options['skip-install'] = true;
+    this.app.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
+  });
+
+  it('creates expected app files', function (done) {
+    var expected = [
+      'bower.json',
+      'package.json',
+      'Gruntfile.js',
+      'karma.conf.js',
+      'README.md',
+
+      'app/index.html'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      'websiteName': 'Some website'
     });
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
